092a6d150320a23c9649491aa772e2ead35c009f
Nicholas Ruffing <nicholasruffing70@gmail.com>
2019-12-21 16:16:34 -0500

    HueBridgeMetadata tests

---
+++ FluentHue.Tests/Contracts/HueBridgeMetadataTests.cs
@@ -0,0 +1,30 @@
+﻿namespace FluentHue.Tests.Contracts
+{
+    using FluentHue.Contracts;
+    using FluentHue.Serializers;
+    using NUnit.Framework;
+    using RestSharp;
+    using RestSharp.Serialization;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [TestFixture]
+    public sealed class HueBridgeMetadataTests
+    {
+        [Test]
+        public void DeserializationTest()
+        {
+            IRestResponse response = new RestResponse();
+            response.Content = "[{\"id\":\"001788fffe4c2912\",\"internalipaddress\":\"192.168.1.7\"}]";
+
+            IRestSerializer serializer = new JsonNetSerializer();
+            var deserialized = serializer.Deserialize<IEnumerable<HueBridgeMetadata>>(response);
+            Assert.NotNull(deserialized);
+            Assert.AreEqual(1, deserialized.Count());
+
+            var metadata = deserialized.First();
+            Assert.AreEqual("001788fffe4c2912", metadata.Id);
+            Assert.AreEqual("192.168.1.7", metadata.InternalIpAddress);
+        }
+    }
+}
