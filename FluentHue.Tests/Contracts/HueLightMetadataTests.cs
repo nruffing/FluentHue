38b5bd5316abee1cf5a2bb757e36dd0757000deb
Nicholas Ruffing <nicholasruffing70@gmail.com>
2020-02-16 14:11:24 -0500

    HueLightMetadata unit tests

---
+++ FluentHue.Tests/Contracts/HueLightMetadataTests.cs
@@ -0,0 +1,101 @@
+﻿using FluentHue.Contracts;
+using FluentHue.Serializers;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentHue.Tests.Contracts
+{
+    [TestFixture]
+    public sealed class HueLightMetadataTests
+    {
+        /* 
+            {
+                "state": {
+                        "on": false,
+                    "bri": 254,
+                    "hue": 48016,
+                    "sat": 102,
+                    "effect": "none",
+                    "xy": [
+                        0.2999,
+                        0.2498
+                    ],
+                    "ct": 153,
+                    "alert": "select",
+                    "colormode": "xy",
+                    "mode": "homeautomation",
+                    "reachable": true
+                },
+                "swupdate": {
+                    "state": "noupdates",
+                    "lastinstall": "2017-12-03T15:49:47"
+                },
+                "type": "Extended color light",
+                "name": "Color-Bedroom",
+                "modelid": "LCT014",
+                "manufacturername": "Philips",
+                "productname": "Hue color lamp",
+                "capabilities": {
+                    "certified": true,
+                    "control": {
+                        "mindimlevel": 1000,
+                        "maxlumen": 800,
+                        "colorgamuttype": "C",
+                        "colorgamut": [
+                            [
+                                0.6915,
+                                0.3083
+                            ],
+                            [
+                                0.1700,
+                                0.7000
+                            ],
+                            [
+                                0.1532,
+                                0.0475
+                            ]
+                        ],
+                        "ct": {
+                            "min": 153,
+                            "max": 500
+                        }
+                    },
+                    "streaming": {
+                        "renderer": true,
+                        "proxy": true
+                    }
+                },
+                "config": {
+                    "archetype": "sultanbulb",
+                    "function": "mixed",
+                    "direction": "omnidirectional",
+                    "startup": {
+                        "mode": "powerfail",
+                        "configured": true
+                    }
+                },
+                "uniqueid": "00:17:88:01:02:4c:de:b8-0b",
+                "swversion": "1.46.13_r26312",
+                "swconfigid": "56D25448",
+                "productid": "Philips-LCT014-1-A19ECLv4"
+            }
+        */
+
+        [Test]
+        public void DeserializationTest()
+        {
+            IRestResponse response = new RestResponse();
+            response.Content = "{\"state\":{ \"on\":false,\"bri\":254,\"hue\":48016,\"sat\":102,\"effect\":\"none\",\"xy\":[0.2999,0.2498],\"ct\":153,\"alert\":\"select\",\"colormode\":\"xy\",\"mode\":\"homeautomation\",\"reachable\":true},\"swupdate\":{\"state\":\"noupdates\",\"lastinstall\":\"2017-12-03T15:49:47\"},\"type\":\"Extended color light\",\"name\":\"Color-Bedroom\",\"modelid\":\"LCT014\",\"manufacturername\":\"Philips\",\"productname\":\"Hue color lamp\",\"capabilities\":{\"certified\":true,\"control\":{\"mindimlevel\":1000,\"maxlumen\":800,\"colorgamuttype\":\"C\",\"colorgamut\":[[0.6915,0.3083],[0.1700,0.7000],[0.1532,0.0475]],\"ct\":{\"min\":153,\"max\":500}},\"streaming\":{\"renderer\":true,\"proxy\":true}},\"config\":{\"archetype\":\"sultanbulb\",\"function\":\"mixed\",\"direction\":\"omnidirectional\",\"startup\":{\"mode\":\"powerfail\",\"configured\":true}},\"uniqueid\":\"00:17:88:01:02:4c:de:b8-0b\",\"swversion\":\"1.46.13_r26312\",\"swconfigid\":\"56D25448\",\"productid\":\"Philips-LCT014-1-A19ECLv4\"}";
+
+            IRestSerializer serializer = new JsonNetSerializer();
+            var deserialized = serializer.Deserialize<HueLightMetadata>(response);
+            Assert.NotNull(deserialized);
+
+            Assert.AreEqual("Color-Bedroom", deserialized.Name);
+        }
+    }
+}
