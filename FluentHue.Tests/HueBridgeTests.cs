b257e758e384783dadecb06690234f38f180f8e5
Nicholas Ruffing <nicholasruffing70@gmail.com>
2020-02-29 20:57:44 -0500

    Additional unit test for HueBridge

--- FluentHue.Tests/HueBridgeTests.cs
+++ FluentHue.Tests/HueBridgeTests.cs
@@ -170,5 +170,231 @@
             Assert.AreEqual(expected.Id, (light as HueLight).Id);
             Assert.AreSame(bridge, light.End());
         }
+
+        [Test]
+        public void GetAllLights_NotSuccessfulThrows()
+        {
+            var bridge = CreateMockBridge()
+                .WithUser(Guid.NewGuid().ToString());
+
+            Mock.Get(Client)
+                .Setup(c => c.ExecuteAsync(
+                    It.IsAny<IRestRequest>(),
+                    It.IsAny<Method>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<IRestResponse>(
+                    new RestResponse()
+                    {
+                        StatusCode = HttpStatusCode.Unauthorized,
+                        ResponseStatus = ResponseStatus.Completed
+                    }));
+
+            Assert.ThrowsAsync(typeof(Exception), async () => await bridge.GetAllLightsAsync().ConfigureAwait(false));
+            Assert.Throws<AggregateException>(() => bridge.GetAllLights());
+        }
+
+        [Test]
+        public async Task DiscoverNewLights_NoSerialNumbers()
+        {
+            var bridge = CreateMockBridge()
+                .WithUser(Guid.NewGuid().ToString());
+
+            Mock.Get(Client)
+                .Setup(c => c.ExecuteAsync(
+                    It.IsAny<IRestRequest>(),
+                    It.IsAny<Method>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<IRestResponse>(
+                    new RestResponse()
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        ResponseStatus = ResponseStatus.Completed
+                    }))
+                .Callback<IRestRequest, Method, CancellationToken>((r, m, t) =>
+                {
+                    Assert.False(r.Parameters.Any());
+                    Assert.AreEqual(Method.POST, m);
+                });
+
+            var result = await bridge.DiscoverNewLightsAsync().ConfigureAwait(false);
+            Assert.AreSame(bridge, result);
+
+            result = bridge.DiscoverNewLights();
+            Assert.AreSame(bridge, result);
+        }
+
+        [Test]
+        public async Task DiscoverNewLights_WithSerialNumbers()
+        {
+            var serialNumbers = new string[]
+            {
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString(),
+            };
+
+            var bridge = CreateMockBridge()
+                .WithUser(Guid.NewGuid().ToString());
+
+            Mock.Get(Client)
+                .Setup(c => c.ExecuteAsync(
+                    It.IsAny<IRestRequest>(),
+                    It.IsAny<Method>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<IRestResponse>(
+                    new RestResponse()
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        ResponseStatus = ResponseStatus.Completed
+                    }))
+                .Callback<IRestRequest, Method, CancellationToken>((r, m, t) =>
+                {
+                    Assert.True(r.Parameters.Any());
+                    Assert.AreEqual(serialNumbers, ((DiscoverNewHueLightsContract)r.Parameters[0].Value).SerialNumbers);
+                    Assert.AreEqual(Method.POST, m);
+                });
+
+            var result = await bridge.DiscoverNewLightsAsync(serialNumbers).ConfigureAwait(false);
+            Assert.AreSame(bridge, result);
+
+            result = bridge.DiscoverNewLights(serialNumbers);
+            Assert.AreSame(bridge, result);
+        }
+
+        [Test]
+        public void DiscoverNewLights_NotSuccessfulThrows()
+        {
+            var bridge = CreateMockBridge()
+                .WithUser(Guid.NewGuid().ToString());
+
+            Mock.Get(Client)
+                .Setup(c => c.ExecuteAsync(
+                    It.IsAny<IRestRequest>(),
+                    It.IsAny<Method>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<IRestResponse>(
+                    new RestResponse()
+                    {
+                        StatusCode = HttpStatusCode.Unauthorized,
+                        ResponseStatus = ResponseStatus.Completed
+                    }));
+
+            Assert.ThrowsAsync(typeof(Exception), async () => await bridge.DiscoverNewLightsAsync().ConfigureAwait(false));
+            Assert.Throws<AggregateException>(() => bridge.DiscoverNewLights());
+        }
+
+        [Test]
+        public void DiscoverNewLights_NoUserSet()
+        {
+            HueBridge bridge = CreateMockBridge();
+
+            Assert.Multiple(() =>
+            {
+                Assert.ThrowsAsync(typeof(ArgumentException), async () => await bridge.DiscoverNewLightsAsync().ConfigureAwait(false));
+                Assert.Throws<AggregateException>(() => bridge.DiscoverNewLights());
+            });
+        }
+
+        [Test]
+        public void SelectLight_NoUserSet()
+        {
+            var light = Guid.NewGuid().ToString();
+
+            HueBridge bridge = CreateMockBridge();
+
+            Assert.Multiple(() =>
+            {
+                Assert.ThrowsAsync(typeof(ArgumentException), async () => await bridge.SelectLightAsync(light).ConfigureAwait(false));
+                Assert.Throws<AggregateException>(() => bridge.SelectLight(light));
+            });
+        }
+
+        [Test]
+        public void SelectLight_NotSuccessfulThrows()
+        {
+            var light = Guid.NewGuid().ToString();
+
+            var bridge = CreateMockBridge()
+                .WithUser(Guid.NewGuid().ToString());
+
+            Mock.Get(Client)
+                .Setup(c => c.ExecuteAsync(
+                    It.IsAny<IRestRequest>(),
+                    It.IsAny<Method>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<IRestResponse>(
+                    new RestResponse()
+                    {
+                        StatusCode = HttpStatusCode.Unauthorized,
+                        ResponseStatus = ResponseStatus.Completed
+                    }));
+
+            Assert.ThrowsAsync(typeof(Exception), async () => await bridge.SelectLightAsync(light).ConfigureAwait(false));
+            Assert.Throws<AggregateException>(() => bridge.SelectLight(light));
+        }
+
+        [Test]
+        public async Task SelectLight()
+        {
+            var bridge = CreateMockBridge()
+                .WithUser(Guid.NewGuid().ToString());
+
+            var expected = new HueLight((HueBridge)bridge, Guid.NewGuid().ToString(), new HueLightContract()
+            {
+                Name = Guid.NewGuid().ToString(),
+            });
+
+            Mock.Get(Client)
+                .Setup(c => c.ExecuteAsync(
+                    It.IsAny<IRestRequest>(),
+                    It.IsAny<Method>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<IRestResponse>(
+                    new RestResponse()
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        ResponseStatus = ResponseStatus.Completed,
+                        Content = $"{{\"{expected.Id}\": {{\"name\": \"{expected.Name}\"}}}}",
+                    }));
+
+            var light = await bridge.SelectLightAsync(expected.Name).ConfigureAwait(false);
+            Assert.NotNull(light);
+            Assert.AreEqual(expected.Name, light.Name);
+            Assert.AreEqual(expected.Id, (light as HueLight).Id);
+            Assert.AreSame(bridge, light.End());
+
+            light = bridge.SelectLight(expected.Name);
+            Assert.NotNull(light);
+            Assert.AreEqual(expected.Name, light.Name);
+            Assert.AreEqual(expected.Id, (light as HueLight).Id);
+            Assert.AreSame(bridge, light.End());
+        }
+
+        [Test]
+        public void SelectLight_LightDoesntExist()
+        {
+            var bridge = CreateMockBridge()
+                .WithUser(Guid.NewGuid().ToString());
+
+            var expected = new HueLight((HueBridge)bridge, Guid.NewGuid().ToString(), new HueLightContract()
+            {
+                Name = Guid.NewGuid().ToString(),
+            });
+
+            Mock.Get(Client)
+                .Setup(c => c.ExecuteAsync(
+                    It.IsAny<IRestRequest>(),
+                    It.IsAny<Method>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<IRestResponse>(
+                    new RestResponse()
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        ResponseStatus = ResponseStatus.Completed,
+                        Content = $"{{\"{expected.Id}\": {{\"name\": \"{expected.Name}\"}}}}",
+                    }));
+
+            Assert.ThrowsAsync(typeof(InvalidOperationException), async () => await bridge.SelectLightAsync(Guid.NewGuid().ToString()).ConfigureAwait(false));
+            Assert.Throws<AggregateException>(() => bridge.SelectLight(Guid.NewGuid().ToString()));
+        }
     }
 }
